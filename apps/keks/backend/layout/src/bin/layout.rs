6ec8ceabd709fa55ac3969b5adebd3688b360d02
Mike Moran <mike@houseofmoran.com>
2024-09-23 21:25:35 +0100

    drawing of original pieces in layout i.e. not just rectangles
    - note that the layour algorithm appears to be rotating some rectangles

--- apps/keks/backend/layout/src/bin/layout.rs
+++ apps/keks/backend/layout/src/bin/layout.rs
@@ -1,11 +1,11 @@
-use std::{fs::File, io::BufReader, path::PathBuf};
+use std::{collections::HashMap, fs::File, io::{BufReader, Cursor}, path::PathBuf};
 
 use binpack2d::{bin_new, Dimension, Rectangle};
 use clap::{command, Parser};
 use conversion::projection::Projection;
-use geo::{BoundingRect, Geometry, GeometryCollection};
+use geo::{BoundingRect, Geometry, GeometryCollection, Polygon};
 use geozero::{geo_types::GeoWriter, geojson::GeoJsonReader, GeozeroDatasource};
-use tiny_skia::Pixmap;
+use image::{ImageReader, RgbaImage};
 
 #[derive(clap:: ValueEnum, Clone, Debug)]
 enum BinType {
@@ -54,7 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Geometry::GeometryCollection(collection) = writer.take_geometry().unwrap() {
         println!("Found {} geometries", collection.len());
 
-        let (dimensions, projection) = create_dimensions(&collection);
+        let (dimensions, projection, identified) = create_dimensions(&collection);
 
         let bounds = collection.bounding_rect().unwrap();
         let (width, height) = (
@@ -65,39 +65,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let (inserted, rejected) = bin.insert_list(&dimensions);
         println!("Inserted: {} Rejected: {}", inserted.len(), rejected.len());
 
-        let pixmap = draw_layout(&inserted, width as u32, height as u32)?;
-        pixmap.save_png(args.layout)?;
+        let image = draw_layout(&inserted, &projection, &identified)?;
+        image.save(args.layout)?;
     }
 
     Ok(())
 }
 
-fn create_dimensions(collection: &GeometryCollection) -> (Vec<Dimension>, Projection) {
+fn create_dimensions(collection: &GeometryCollection) -> (Vec<Dimension>, Projection, HashMap<isize, &Polygon>) {
     let scale = 10000.0;
     let projection = Projection::from_geo_bounding_box_to_scaled_space(collection.bounding_rect().unwrap(), scale);
+    let mut identified = HashMap::new();
 
     let mut dimensions = vec![];
-    for (id, geometry) in collection.iter().enumerate() {
+    for (index, geometry) in collection.iter().enumerate() {
         if let Geometry::Polygon(polygon) = geometry {
             let bounds = polygon.bounding_rect().unwrap();
+            let id = index as isize;
             let dimension = Dimension::with_id(
-                id as isize, 
+                id, 
                 (bounds.width() * projection.scale_x) as i32, 
                 (bounds.height() * projection.scale_y) as i32, 
                 0);
             dimensions.push(dimension);
+            identified.insert(id, polygon);
         }
     }
-    (dimensions, projection)
+    (dimensions, projection, identified)
 }
 
-fn draw_layout(rects: &Vec<Rectangle>, width: u32, height: u32) -> Result<Pixmap, Box<dyn std::error::Error>> {
+fn draw_layout(rects: &Vec<Rectangle>, projection: &Projection, identified: &HashMap<isize, &Polygon>) -> Result<RgbaImage, Box<dyn std::error::Error>> {
     use tiny_skia::*;
 
-    let max_x = rects.iter().map(|rect| rect.x() + rect.width()).max().unwrap() as u32;
-    let max_y = rects.iter().map(|rect| rect.y() + rect.height()).max().unwrap() as u32;
+    let max_x = rects.iter().map(|rect| rect.x() + rect.width()).max().unwrap();
+    let max_y = rects.iter().map(|rect| rect.y() + rect.height()).max().unwrap();
 
-    let mut pixmap = Pixmap::new(max_x, max_y).ok_or("Failed to create pixmap")?;
+    let mut pixmap = Pixmap::new(max_x as u32, max_y as u32).ok_or("Failed to create pixmap")?;
 
     let mut black = Paint::default();
     black.set_color(Color::BLACK);
@@ -107,11 +110,15 @@ fn draw_layout(rects: &Vec<Rectangle>, width: u32, height: u32) -> Result<Pixmap
     white.set_color(Color::WHITE);
     white.anti_alias = true;
 
+    let mut red = Paint::default();
+    red.set_color_rgba8(255, 0, 0, 255);
+    red.anti_alias = true;
+
     let mut stroke = Stroke::default();
-    stroke.width = 0.005 * (width as f32).min(height as f32);
+    stroke.width = 0.0005 * (max_x as f32).min(max_y as f32);
 
     pixmap.fill_rect(
-        Rect::from_xywh(0.0, 0.0, width as f32, height as f32).ok_or("Failed to create rect")?,
+        Rect::from_xywh(0.0, 0.0, max_x as f32, max_y as f32).ok_or("Failed to create rect")?,
         &black,
         Transform::identity(),
         None
@@ -125,8 +132,32 @@ fn draw_layout(rects: &Vec<Rectangle>, width: u32, height: u32) -> Result<Pixmap
         pb.line_to(rect.x() as f32, rect.y() as f32 + rect.height() as f32);
         pb.close();
         let path = pb.finish().ok_or("Failed to finish path")?;
-        pixmap.stroke_path(&path, &white, &stroke, Transform::identity(), None);
+        pixmap.stroke_path(&path, &red, &stroke, Transform::identity(), None);
+
+        let id = rect.id();
+        let polygon = identified.get(&id).ok_or(format!("Failed to find geometry for id {}", id))?;
+        let mut pb = PathBuilder::new();
+        let bounds = polygon.bounding_rect().unwrap();
+        polygon.exterior().points().for_each(|p| {
+            let (x, y) = (
+                (p.x() - bounds.min().x) * projection.scale_x + rect.x() as f64,
+                (p.y() - bounds.min().y) * projection.scale_y + rect.y() as f64
+            );
+            if pb.is_empty() {
+                pb.move_to(x as f32, y as f32);
+            } else {
+                pb.line_to(x as f32, y as f32);
+            }
+        });
+        pb.close();
+        let path = pb.finish().ok_or("Failed to finish path")?;
+        pixmap.fill_path(&path, &white, FillRule::EvenOdd, Transform::identity(), None);
     }
 
-    Ok(pixmap)
+    let png_bytes = pixmap.encode_png()?;
+    let mut reader = ImageReader::new(Cursor::new(png_bytes));
+    reader.set_format(image::ImageFormat::Png);
+    let decoded = reader.decode()?;
+
+    Ok(decoded.into_rgba8())
 }
