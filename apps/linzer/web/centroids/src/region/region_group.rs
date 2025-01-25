46f0fb5c9e5ef986c29d9cf1862af973b0d5f451
Mike Moran <mike@houseofmoran.com>
2025-01-25 15:26:43 +0000

    add some tests for `RegionGroup` before I optimise it

--- apps/linzer/web/centroids/src/region/region_group.rs
+++ apps/linzer/web/centroids/src/region/region_group.rs
@@ -22,3 +22,45 @@ impl RegionGroup {
         geometries
     }
 }
+
+#[cfg(test)]
+mod test {
+    use geo::{line_string, polygon, Geometry, GeometryCollection, Point, Polygon};
+
+    use crate::region::region_group::RegionGroup;
+
+    #[test]
+    fn test_takes_generic_geometries_and_supplies_polygons_centroids_and_identifiers() {
+        let polygon: Polygon<f64> = polygon![
+            (x: 0.0, y: 0.0),
+            (x: 1.0, y: 0.0),
+            (x: 1.0, y: 1.0),
+            (x: 0.0, y: 1.0),
+            (x: 0.0, y: 0.0)
+        ];
+        let geometry = Geometry::Polygon(polygon);
+        let collection = GeometryCollection::from(vec![geometry]);
+        let group = RegionGroup::new("test".to_string(), collection);
+        let geometries = group.geometries();
+        assert_eq!(geometries.len(), 1);
+        let (polygon, id, centroid) = &geometries[0];
+        assert_eq!(id, "test-0");
+        assert_eq!(polygon.exterior(), &line_string![
+            (x: 0.0, y: 0.0),
+            (x: 1.0, y: 0.0),
+            (x: 1.0, y: 1.0),
+            (x: 0.0, y: 1.0),
+            (x: 0.0, y: 0.0)
+        ]);
+        assert_eq!(centroid, &Point::new(0.5, 0.5));
+    }
+
+    #[test]
+    fn test_ignores_non_polygon_geometry() {
+        let point = Geometry::Point(Point::new(0.0, 0.0));
+        let collection = GeometryCollection::from(vec![point]);
+        let group = RegionGroup::new("test".to_string(), collection);
+        let geometries = group.geometries();
+        assert_eq!(geometries.len(), 0);
+    }
+}
