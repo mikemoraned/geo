767dd84ce1f74705a498ff96616812138054a056
Mike Moran <mike@houseofmoran.com>
2025-02-08 23:43:30 +0000

    minimal mapbox map display

--- apps/geoarrow.info/webapp/src/routes/+page.svelte
+++ apps/geoarrow.info/webapp/src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+	import mapbox from 'mapbox-gl';
+	import 'mapbox-gl/dist/mapbox-gl.css';
+	const { Map } = mapbox;
+	import { onMount, onDestroy } from 'svelte';
+
+	import { PUBLIC_MAPBOX_TOKEN } from '$env/static/public';
+
+	let map;
+
+	const edinburgh = [-3.188267, 55.953251];
+	const starting_position = {
+		center: edinburgh,
+		zoom: 12
+	};
+
+	onMount(async () => {
+		map = new Map({
+			container: "map-container",
+			accessToken: PUBLIC_MAPBOX_TOKEN,
+			style: `mapbox://styles/mapbox/outdoors-v11`,
+			...starting_position
+		});
+	});
+
+	onDestroy(() => {
+		if (map) {
+			map.remove();
+		}
+	});
+</script>
+
+<head>
+	<title>geoarrow.info</title>
+</head>
+
+<div id="map-container"></div>
+
+<style>
+    #map-container {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+    }
+</style>
