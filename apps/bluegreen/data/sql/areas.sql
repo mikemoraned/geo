7009fd3f8911b382bd3a954f1b5e078ffe2c0880
Mike Moran <mike@houseofmoran.com>
2025-06-15 22:42:07 +0100

    more SQL play

--- apps/bluegreen/data/sql/areas.sql
+++ apps/bluegreen/data/sql/areas.sql
@@ -1,3 +1,9 @@
+SELECT *
+FROM division_area
+WHERE names.primary LIKE '%Venice%'
+
+---
+
 WITH city AS (           
     SELECT geometry
     FROM   division_area
@@ -45,3 +51,33 @@ FROM   city
 -- 100 * 0.0003313394412799 / 0.03784896591359801 = 0.8754253473
 -- Amsterdam:
 -- 100 * 0.004743008080688963 / 0.032147921495904924 = 14.7537005815
+
+---
+
+-- all elements (not just polygons and multipolygons)
+WITH city AS (           
+    SELECT geometry
+    FROM   division_area
+    WHERE  
+    	-- id = '0851d7537fffffff015d4f9f0d8b31e6' -- Berlin
+    	id = '085ba2a9bfffffff01a888f06236016b' -- Edinburgh
+    	-- id = '0850a811bfffffff010c628b3d6d6ee3' -- Amsterdam
+),
+bbox AS (                  
+    SELECT
+        ST_XMin(env) AS xmin,
+        ST_YMin(env) AS ymin,
+        ST_XMax(env) AS xmax,
+        ST_YMax(env) AS ymax
+    FROM (
+        SELECT ST_Envelope(geometry) AS env
+        FROM   city
+    )
+)
+SELECT *
+FROM   base_water        AS w
+CROSS  JOIN bbox         AS bb
+WHERE  (w.bbox).xmin > bb.xmin   
+  AND  (w.bbox).ymin > bb.ymin
+  AND  (w.bbox).xmax < bb.xmax
+  AND  (w.bbox).ymax < bb.ymax
--- apps/bluegreen/data/sql/explore.sql
+++ apps/bluegreen/data/sql/explore.sql
@@ -304,5 +304,13 @@ WHERE names.primary LIKE '%Berlin%'
       AND subtype = 'region'
       
 
+---
+      
+      
+SELECT
+    *
+FROM
+    read_parquet('s3://overturemaps-us-west-2/release/2025-05-21.0/theme=divisions/type=division_area/*', 
+                 hive_partitioning=1)
+WHERE names.primary LIKE '%Venice%'
 
-     
