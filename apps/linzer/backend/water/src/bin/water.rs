53f32b1bf874d20fe82ef526833c5abcbd66ac13
Mike Moran <mike@houseofmoran.com>
2025-07-13 14:10:43 +0100

    add ability to output arrow

--- apps/linzer/backend/water/src/bin/water.rs
+++ apps/linzer/backend/water/src/bin/water.rs
@@ -1,8 +1,13 @@
-use std::{fs::File, io::BufWriter, path::PathBuf};
+use std::{ffi::OsStr, fs::File, io::BufWriter, path::PathBuf, sync::Arc};
 
+use arrow_schema::{Field, Schema};
 use clap::{command, Parser};
 use config::Config;
-use geo::GeometryCollection;
+use geo::geometry;
+use geoarrow::{
+    array::GeometryCollectionBuilder, datatypes::Dimension, io::geozero::ToGeometry,
+    scalar::OwnedGeometry, GeometryArrayTrait,
+};
 use geozero::{geojson::GeoJsonWriter, GeozeroGeometry};
 use overturemaps::overturemaps::WaterHandling;
 use thiserror::Error;
@@ -19,9 +24,11 @@ struct Args {
     #[arg(long)]
     overturemaps: Option<String>,
 
-    /// output GeoJSON `.geojson` file representing the water found
+    /// output a GeoJson or geoarrow file representing the water found
+    /// the format is determined by the file extension
+    /// e.g. `water.geojson` or `water.arrow`
     #[arg(long)]
-    water: PathBuf,
+    water: Vec<PathBuf>,
 
     /// water handling
     #[arg(long, value_enum)]
@@ -48,22 +55,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         if let Some(bounds) = om.find_geometry_by_id(gers_id).await? {
             let water = om.find_water_in_region(&bounds, args.handling).await?;
-            save(&water, &args.water)?;
+            for path in &args.water {
+                save(&water, path)?;
+            }
         }
     }
 
     Ok(())
 }
 
-fn save(geo: &geo::geometry::Geometry, path: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+fn save(
+    geo: &geo::geometry::Geometry<f64>,
+    path: &PathBuf,
+) -> Result<(), Box<dyn std::error::Error>> {
     match geo {
         geo::geometry::Geometry::GeometryCollection(collection) => {
-            let fout = BufWriter::new(File::create(path)?);
-            let mut gout = GeoJsonWriter::new(fout);
-            geo::geometry::Geometry::GeometryCollection(collection.clone())
-                .process_geom(&mut gout)?;
+            match path.extension().and_then(OsStr::to_str) {
+                Some("geojson") => {
+                    let fout = BufWriter::new(File::create(path)?);
+                    let mut gout = GeoJsonWriter::new(fout);
+                    geo::geometry::Geometry::GeometryCollection(collection.clone())
+                        .process_geom(&mut gout)?;
 
-            Ok(())
+                    Ok(())
+                }
+                Some("arrow") => {
+                    let mut builder: GeometryCollectionBuilder<_, 2> =
+                        GeometryCollectionBuilder::new();
+                    for geom in collection.0 {
+                        builder.push_geometry(Some(&geom.to_geometry()?), false);
+                    }
+                    let array = builder.finish();
+
+                    let field = Field::new("geometry", array.data_type().clone(), true);
+                    let schema = Arc::new(Schema::new(vec![field]));
+                    let batch =
+                        RecordBatch::try_new(schema.clone(), vec![Arc::new(array)]).unwrap();
+                    let fout = BufWriter::new(File::create(path)?);
+                    write_ipc_stream(&mut fout, &schema, vec![batch]).unwrap();
+                    Ok(())
+                }
+                _ => Err(Box::new(WaterError::CannotSaveGeometry)),
+            }
         }
 
         _ => Err(Box::new(WaterError::CannotSaveGeometry)),
