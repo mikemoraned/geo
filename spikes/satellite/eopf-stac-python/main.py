3d463943ecba90dc07b47a2aeaf8806d7449cec8
Mike Moran <mike@houseofmoran.com>
2025-07-28 19:55:35 +0200

    connecting to catalog and printing metadata
    - following https://eopf-toolkit.github.io/eopf-101/33_eopf_stac_connection.html

--- spikes/satellite/eopf-stac-python/main.py
+++ spikes/satellite/eopf-stac-python/main.py
@@ -1,6 +1,23 @@
+import requests
+from typing import List, Optional, cast
+from pystac import Collection, MediaType
+from pystac_client import Client, CollectionClient
+from datetime import datetime
+
 def main():
     print("Hello from eopf-stac-python!")
 
+    eopf_stac_api_root_endpoint = "https://stac.core.eopf.eodc.eu/" #root starting point
+    eopf_catalog = Client.open(url=eopf_stac_api_root_endpoint) # calls the selected url
+    print(eopf_catalog.to_dict())
+
+def list_found_elements(search_result):
+    id = []
+    coll = []
+    for item in search_result.items(): #retrieves the result inside the catalogue.
+        id.append(item.id)
+        coll.append(item.collection_id)
+    return id , coll
 
 if __name__ == "__main__":
     main()
