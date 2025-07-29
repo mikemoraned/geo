1321ccb1a2ab9d2abe23c4ab05e25ed0f1009cb8
Mike Moran <mike@houseofmoran.com>
2025-07-29 13:04:35 +0200

    add `esa-python-uv-direct` reading zarr file directly

---
+++ spikes/satellite/esa-python-uv-direct/main.py
@@ -0,0 +1,23 @@
+import zarr
+import fsspec
+
+ENDPOINT_URL = "https://cci-ke-o.s3-ext.jc.rl.ac.uk/"
+BUCKET = "esacci"
+ZARR_FILE = "ESACCI-L3C_SNOW-SWE-1979-2020-fv2.0.zarr"
+
+
+def main():
+    print("Hello from esa-python-uv-direct!")
+
+    fs = fsspec.filesystem(
+        "s3", anon=True, asynchronous=True, client_kwargs={"endpoint_url": ENDPOINT_URL}
+    )
+    store = zarr.storage.FsspecStore(fs, read_only=True, path=f"{BUCKET}/{ZARR_FILE}")
+    z = zarr.open(store, mode="r")
+    print("Available datasets:")
+    for key in z.keys():
+        print(f" - {key}")
+
+
+if __name__ == "__main__":
+    main()
