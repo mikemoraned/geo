903163b0adf972a80c8f37a62f66f5be2c2dfe8a
Mike Moran <mike@houseofmoran.com>
2025-12-23 13:38:27 +0100

    was playing with using GERs ids

--- spikes/mgmSep2025/workspace3/tube.py
+++ spikes/mgmSep2025/workspace3/tube.py
@@ -47,6 +47,27 @@ def _(duckdb):
 
 @app.cell
 def _(gpd, wkt):
+    def load_by_ids(conn, overture_local_base, gers_ids):
+        path = f"{overture_local_base}/theme=divisions/type=division_area/*"
+        gers_ids_joined = ",".join([f"'{g}'" for g in gers_ids])
+        query = f"""
+        SELECT * EXCLUDE(geometry), ST_AsText(geometry) AS geometry 
+        FROM read_parquet('{path}')
+        WHERE id IN ({gers_ids_joined})
+        """
+        arrow_table = conn.execute(query).fetch_arrow_table()
+        df = arrow_table.to_pandas()
+        gdf = gpd.GeoDataFrame(
+            df,
+            geometry=df['geometry'].apply(wkt.loads),
+            crs="EPSG:4326"
+        )
+        return gdf
+    return (load_by_ids,)
+
+
+@app.cell
+def _(gpd, wkt):
     def load_area(conn, overture_local_base, overture_theme, overture_type, bbox):
         (minx,miny,maxx,maxy) = bbox
         path = f"{overture_local_base}/theme={overture_theme}/type={overture_type}/*"
@@ -78,11 +99,9 @@ def _(mo):
 
 
 @app.cell
-def _():
-    # London bbox based on example on https://wiki.openstreetmap.org/wiki/Bounding_box
-    # We could derive this based on the GERS id, but not for now
-    london_bbox = (-0.489,51.28,0.236,51.686)
-    return (london_bbox,)
+def _(duckdb_connection):
+    conn = duckdb_connection()
+    return (conn,)
 
 
 @app.cell
@@ -93,9 +112,33 @@ def _():
 
 
 @app.cell
-def _(duckdb_connection):
-    conn = duckdb_connection()
-    return (conn,)
+def _():
+    gers_ids = [
+        "531326d0-51f4-4c9e-8af5-d704aeea7830", # City of Westminster
+        "89c092f8-4287-4401-b72f-4a5a067eee22", # City of London
+        "5e0a58c5-df70-47c4-a71d-52235d3cb6d5"  # London Borough of Tower Hamlets
+    ] 
+    return (gers_ids,)
+
+
+@app.cell
+def _(conn, gers_ids, load_by_ids, overturemaps_base):
+    london_regions_gdf = load_by_ids(conn, overturemaps_base, gers_ids)
+    return (london_regions_gdf,)
+
+
+@app.cell
+def _(london_regions_gdf):
+    london_regions_gdf.explore()
+    return
+
+
+@app.cell
+def _():
+    # London bbox based on example on https://wiki.openstreetmap.org/wiki/Bounding_box
+    # We could derive this based on the GERS id, but not for now
+    london_bbox = (-0.489,51.28,0.236,51.686)
+    return (london_bbox,)
 
 
 @app.cell
@@ -125,12 +168,12 @@ def _(pd):
     def find_subway_segment_with_connectors(gdf):
         # restrict to only those segments that belong to a subway
         subway_segment_gdf = gdf[gdf["class"] == 'subway']
-    
+
         # each segment has a list of connectors (stops along it)
         # create a dataframe which contains one row for each
         # connector, along with all segment information
         exploded_gdf = subway_segment_gdf.explode("connectors")
-    
+
         # create a new dataframe which contains a new
         # `connector_id` column, but is indexed by the original
         # `exploded_gdf` dataframe
@@ -177,14 +220,9 @@ def _(london_connectors_gdf, london_subway_segment_gdf):
 
 
 @app.cell
-def _(london_connectors_restricted_gdf):
-    london_connectors_restricted_gdf.explore()
-    return
-
-
-@app.cell
-def _(london_connectors_restricted_gdf):
-    london_connectors_restricted_gdf.explore(tiles="CartoDB positron")
+def _(london_connectors_restricted_gdf, london_regions_gdf):
+    m = london_connectors_restricted_gdf.explore(tiles="CartoDB positron")
+    london_regions_gdf.explore(m=m)
     return
 
 
