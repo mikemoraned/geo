d698b6de3c7da8a82a9cccfe4ee9e2e6833acf1f
Mike Moran <mike@houseofmoran.com>
2025-12-23 20:53:13 +0100

    get subways from base infra dataset

--- spikes/mgmSep2025/workspace3/tube.py
+++ spikes/mgmSep2025/workspace3/tube.py
@@ -13,9 +13,22 @@ def _():
 @app.cell
 def _(mo):
     mo.md(r"""
-    # Goal
+    # Goals
 
-    Show all London subway stations
+    * Find all London subway stations
+    * Explore:
+        * OvertureMaps `segments`/`connector` transport dataset, as well as `base` dataset
+        * Using GeoPandas / Marimo / Duckdb Spatial
+
+    ## Approach
+
+    Assume that connectors between subways segments are tube station.
+
+    ## Conclusion
+
+    Using "segments"/"connector" datasets I think what I've actually found is not subway stations, but instead subway platforms, including some that appear to be under the water 😀.
+
+    Whilst it was fun to play with the transport data, it is far simpler to use the `base` `infrastructure` dataset with a filter of `subtype == transit` and `class == subway_station`.
     """)
     return
 
@@ -69,6 +82,24 @@ def _(gpd, wkt):
     return (load_area,)
 
 
+@app.cell
+def _():
+    from shapely.geometry import box
+
+    def overlapping_bbox(gdf, bbox):
+        (minx,miny,maxx,maxy) = bbox
+    
+        overlapping_gdf = gdf[gdf.intersects(box(minx, miny, maxx, maxy))]
+
+        return overlapping_gdf
+
+    def centroid_as_latlon(bbox):
+        (minx,miny,maxx,maxy) = bbox
+        centroid = box(minx, miny, maxx, maxy).centroid
+        return [centroid.y, centroid.x]
+    return centroid_as_latlon, overlapping_bbox
+
+
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
@@ -86,16 +117,22 @@ def _(duckdb_connection):
 @app.cell
 def _():
     overturemaps_release = "2025-12-17.0"
-    overturemaps_base = f"/Volumes/PRO-G40/OvertureMaps/data/release/{overturemaps_release}"
+    overturemaps_base = f"/Volumes/PRO-G40/OvertureMaps/data/release/{overturemaps_release}" # this is a local copy I have on a drive
     return (overturemaps_base,)
 
 
 @app.cell
 def _():
     # London bbox based on example on https://wiki.openstreetmap.org/wiki/Bounding_box
-    # We could derive this based on the GERS id, but not for now
     london_bbox = (-0.489,51.28,0.236,51.686)
     return (london_bbox,)
+
+
+@app.cell
+def _():
+    # interactively selected in https://boundingbox.klokantech.com
+    kingcross_area_bbox = (-0.138,51.527,-0.118,51.533)
+    return (kingcross_area_bbox,)
 
 
 @app.cell
@@ -105,8 +142,15 @@ def _(conn, load_area, london_bbox, overturemaps_base):
 
 
 @app.cell
-def _(london_segments_gdf):
-    london_segments_gdf.head()
+def _(
+    centroid_as_latlon,
+    kingcross_area_bbox,
+    london_segments_gdf,
+    overlapping_bbox,
+):
+    # transport segments overlapping kings cross area
+    overlapping_bbox(london_segments_gdf, kingcross_area_bbox).explore(
+        tiles="CartoDB positron", location=centroid_as_latlon(kingcross_area_bbox), zoom_start=15)
     return
 
 
@@ -147,14 +191,25 @@ def _(pd):
 @app.cell
 def _(find_subway_segment_with_connectors, london_segments_gdf):
     london_subway_segment_gdf = find_subway_segment_with_connectors(london_segments_gdf)
-    london_subway_segment_gdf.head()
     return (london_subway_segment_gdf,)
+
+
+@app.cell
+def _(
+    centroid_as_latlon,
+    kingcross_area_bbox,
+    london_subway_segment_gdf,
+    overlapping_bbox,
+):
+    # segments overlapping kings cross area
+    overlapping_bbox(london_subway_segment_gdf, kingcross_area_bbox).explore(
+        tiles="CartoDB positron", location=centroid_as_latlon(kingcross_area_bbox), zoom_start=15)
+    return
 
 
 @app.cell
 def _(conn, load_area, london_bbox, overturemaps_base):
     london_connectors_gdf = load_area(conn, overturemaps_base, "transportation", "connector", london_bbox)
-    london_connectors_gdf.head()
     return (london_connectors_gdf,)
 
 
@@ -172,13 +227,106 @@ def restrict_to_connectors(connector_gdf, subset_gdf):
 @app.cell
 def _(london_connectors_gdf, london_subway_segment_gdf):
     london_connectors_restricted_gdf = restrict_to_connectors(london_connectors_gdf, london_subway_segment_gdf)
-    london_connectors_restricted_gdf.head()
     return (london_connectors_restricted_gdf,)
 
 
 @app.cell
 def _(london_connectors_restricted_gdf):
-    london_connectors_restricted_gdf.explore(tiles="CartoDB positron")
+    london_connectors_restricted_gdf.explore(tiles="CartoDB positron", color="red")
+    return
+
+
+@app.cell
+def _(
+    centroid_as_latlon,
+    kingcross_area_bbox,
+    london_connectors_restricted_gdf,
+    london_subway_segment_gdf,
+    overlapping_bbox,
+):
+    m = overlapping_bbox(london_subway_segment_gdf, kingcross_area_bbox).explore(
+        tiles="CartoDB positron", location=centroid_as_latlon(kingcross_area_bbox), zoom_start=16, color="blue")
+    overlapping_bbox(london_connectors_restricted_gdf, kingcross_area_bbox).explore(m=m, color="red")
+    return
+
+
+@app.cell
+def _(
+    london_connectors_restricted_gdf,
+    london_subway_segment_gdf,
+    overlapping_bbox,
+):
+    embankment_bbox = (-0.1252405125,51.5040095967,-0.1151554066,51.5083268675)
+    embankment = overlapping_bbox(london_subway_segment_gdf, embankment_bbox).explore(
+        tiles="CartoDB positron", location=[51.5061, -0.1216], zoom_start=17, color="blue")
+    overlapping_bbox(london_connectors_restricted_gdf, embankment_bbox).explore(m=embankment, color="red")
+    return (embankment_bbox,)
+
+
+@app.cell
+def _(conn, load_area, london_bbox, overturemaps_base):
+    # try using base, guided by https://docs.overturemaps.org/schema/concepts/by-theme/base/
+    london_infra_gdf = load_area(conn, overturemaps_base, "base", "infrastructure", london_bbox)
+    return (london_infra_gdf,)
+
+
+@app.cell
+def _(london_infra_gdf):
+    london_infra_gdf.head()
+    return
+
+
+@app.cell
+def _(
+    centroid_as_latlon,
+    kingcross_area_bbox,
+    london_infra_gdf,
+    overlapping_bbox,
+):
+    # infra overlapping kings cross area
+    overlapping_bbox(london_infra_gdf, kingcross_area_bbox).explore(
+        tiles="CartoDB positron", location=centroid_as_latlon(kingcross_area_bbox), zoom_start=16, color="green")
+    return
+
+
+@app.cell
+def _(london_infra_gdf):
+    london_infra_subway_gdf = london_infra_gdf[
+        (london_infra_gdf['subtype'] == 'transit') 
+        & (london_infra_gdf['class'] == 'subway_station')]
+    return (london_infra_subway_gdf,)
+
+
+@app.cell
+def _(
+    centroid_as_latlon,
+    kingcross_area_bbox,
+    london_infra_subway_gdf,
+    overlapping_bbox,
+):
+    # subways overlapping kings cross area
+    overlapping_bbox(london_infra_subway_gdf, kingcross_area_bbox).explore(
+        tiles="CartoDB positron", location=centroid_as_latlon(kingcross_area_bbox), zoom_start=16, color="green")
+    return
+
+
+@app.cell
+def _(
+    centroid_as_latlon,
+    embankment_bbox,
+    london_infra_subway_gdf,
+    overlapping_bbox,
+):
+    # subways overlapping embankment area
+    overlapping_bbox(london_infra_subway_gdf, embankment_bbox).explore(
+        tiles="CartoDB positron", location=centroid_as_latlon(embankment_bbox), zoom_start=16, color="green")
+    return
+
+
+@app.cell
+def _(london_infra_subway_gdf):
+    # subways of london
+    london_infra_subway_gdf.explore(tiles="CartoDB positron", color="green")
     return
 
 
